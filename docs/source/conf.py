50d3eba52136673412da29fe3a6ef697b76fe5ee
Till Hainbach <till.hainbach@uni-potsdam.de>
2020-05-14 12:33:36 +0200

    Add private members and change font-family

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,12 @@ release = '0.0.1.dev'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.napoleon',
-        'sphinx_markdown_builder'
+    'sphinx.ext.napoleon',
+    'sphinx_markdown_builder'
 ]
+
+# Napoleon settings
+napoleon_include_private_with_doc = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,9 +52,20 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+font_family = 'Verdana'
 html_theme = 'alabaster'
+html_theme_options = {
+    'github_user': 'tillhainbach',
+    'github_repo': 'pyansiescapes',
+    'font_family': font_family,
+    'caption_font_family': font_family,
+    'head_font_family': font_family,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
